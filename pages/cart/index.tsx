6d53ee1d20fce220d620c87014a855a3bfc2c512
Stanly Calle <callestanly@gmail.com>
2022-11-30 15:10:30 -0500

    Eliminada pagina /cart/empty, se unifico el carrito lleno y vacio en la misma pagina

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react';
-import { NextPage } from 'next';import { ShoppingCartFull } from '../../components/cart';
+import { NextPage } from 'next';import { ShoppingCartEmpty, ShoppingCartFull } from '../../components/cart';
 import { useRouter } from 'next/router';
 
 import { ShopLayout } from '../../components/layouts';
@@ -10,21 +10,18 @@ import { CartContext } from '../../context/cart';
 const CartPage: NextPage = () => {
 
   const { isLoaded, summary: { numberOfItems } } = useContext( CartContext )
-  const router = useRouter();
 
-  useEffect(() => {
-
-    if ( isLoaded && numberOfItems === 0 ) router.replace('/cart/empty')
-
-  }, [ isLoaded, numberOfItems, router ]);
-
-  if ( !isLoaded || numberOfItems === 0 ) {
-    return <></>;
+  if ( isLoaded && numberOfItems > 0 ) {
+    return (
+      <ShopLayout title={`Carrito de compras - ${ numberOfItems } ${numberOfItems <= 1 ? 'producto' : 'productos'}`} description='Carrito de compras de la tienda'>
+        <ShoppingCartFull />    
+      </ShopLayout>
+    )
   }
 
   return (
-    <ShopLayout title={`Carrito de compras - ${ numberOfItems } ${numberOfItems <= 1 ? 'producto' : 'productos'}`} description='Carrito de compras de la tienda'>
-        <ShoppingCartFull />
+    <ShopLayout title='Carrito de compras' description='No hay articulos en el carrito de compras'>
+      <ShoppingCartEmpty />
     </ShopLayout>
   )
 }
